1fbdef605c3a61126dd29dc323d80847f6a6a616
Alex <ajoshua14@icloud.com>
2023-07-10 15:45:08 -0700

    Conform placeholder data to expected format

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -1,83 +1,185 @@
-import { Song } from "../types/spotifyTypes";
+import { SpotifyItem } from "../types/spotifyTypes";
 
-export const songs: Song[] = [
+export const songs: SpotifyItem[] = [
   {
     id: 1,
     name: "Hello Hello Hello",
-    artists: [{name: "Remi Wolf"}],
+    artists: [{name: "Remi Wolf", href: "", id: "", type: "", uri: ""}],
     album: {
       name: "I'm Allergic to Dogs!",
       images: [
         {
           url: "https://i.scdn.co/image/ab67616d0000b2730e68fefa1408603f54a4d664",
+          width: 640,
+          height: 640,
         }
-      ]
+      ],
+      album_type: "",
+      artists: [{ name: "Remi Wolf", href: "", id: "", type: "", uri: "" }],
+      release_date: "",
+      total_tracks: 10,
+      external_urls: { spotify: "" },
+      uri: "",
+      href: "",
+      id: "",
+      type: ""
     },
+    duration_ms: 1000,
+    explicit: false,
+    href: "",
+    uri: "",
+    type: "song",
+    popularity: 100,
   },
   {
     id: 2,
     name: "Hello Hello Hello",
-    artists: [{name: "Remi Wolf"}],
+    artists: [{name: "Remi Wolf", href: "", id: "", type: "", uri: ""}],
     album: {
       name: "I'm Allergic to Dogs!",
       images: [
         {
           url: "https://i.scdn.co/image/ab67616d0000b2730e68fefa1408603f54a4d664",
+          width: 640,
+          height: 640,
         }
-      ]
+      ],
+      album_type: "",
+      artists: [{ name: "Remi Wolf", href: "", id: "", type: "", uri: "" }],
+      release_date: "",
+      total_tracks: 10,
+      external_urls: { spotify: "" },
+      uri: "",
+      href: "",
+      id: "",
+      type: ""
     },
+    duration_ms: 1000,
+    explicit: false,
+    href: "",
+    uri: "",
+    type: "song",
+    popularity: 100,
   },
   {
     id: 3,
-    name: "Hello Hello Hello",
-    artists: [{name: "Remi Wolf"}],
+   name: "Hello Hello Hello",
+    artists: [{name: "Remi Wolf", href: "", id: "", type: "", uri: ""}],
     album: {
       name: "I'm Allergic to Dogs!",
       images: [
         {
           url: "https://i.scdn.co/image/ab67616d0000b2730e68fefa1408603f54a4d664",
+          width: 640,
+          height: 640,
         }
-      ]
+      ],
+      album_type: "",
+      artists: [{ name: "Remi Wolf", href: "", id: "", type: "", uri: "" }],
+      release_date: "",
+      total_tracks: 10,
+      external_urls: { spotify: "" },
+      uri: "",
+      href: "",
+      id: "",
+      type: ""
     },
+    duration_ms: 1000,
+    explicit: false,
+    href: "",
+    uri: "",
+    type: "song",
+    popularity: 100,
   },
   {
     id: 4,
-    name: "Hello Hello Hello",
-    artists: [{name: "Remi Wolf"}],
+   name: "Hello Hello Hello",
+    artists: [{name: "Remi Wolf", href: "", id: "", type: "", uri: ""}],
     album: {
       name: "I'm Allergic to Dogs!",
       images: [
         {
           url: "https://i.scdn.co/image/ab67616d0000b2730e68fefa1408603f54a4d664",
+          width: 640,
+          height: 640,
         }
-      ]
+      ],
+      album_type: "",
+      artists: [{ name: "Remi Wolf", href: "", id: "", type: "", uri: "" }],
+      release_date: "",
+      total_tracks: 10,
+      external_urls: { spotify: "" },
+      uri: "",
+      href: "",
+      id: "",
+      type: ""
     },
+    duration_ms: 1000,
+    explicit: false,
+    href: "",
+    uri: "",
+    type: "song",
+    popularity: 100,
   },
   {
     id: 5,
-    name: "Hello Hello Hello",
-    artists: [{name: "Remi Wolf"}],
+   name: "Hello Hello Hello",
+    artists: [{name: "Remi Wolf", href: "", id: "", type: "", uri: ""}],
     album: {
       name: "I'm Allergic to Dogs!",
       images: [
         {
           url: "https://i.scdn.co/image/ab67616d0000b2730e68fefa1408603f54a4d664",
+          width: 640,
+          height: 640,
         }
-      ]
+      ],
+      album_type: "",
+      artists: [{ name: "Remi Wolf", href: "", id: "", type: "", uri: "" }],
+      release_date: "",
+      total_tracks: 10,
+      external_urls: { spotify: "" },
+      uri: "",
+      href: "",
+      id: "",
+      type: ""
     },
+    duration_ms: 1000,
+    explicit: false,
+    href: "",
+    uri: "",
+    type: "song",
+    popularity: 100,
   },
   {
     id: 6,
-    name: "Hello Hello Hello",
-    artists: [{name: "Remi Wolf"}],
+   name: "Hello Hello Hello",
+    artists: [{name: "Remi Wolf", href: "", id: "", type: "", uri: ""}],
     album: {
       name: "I'm Allergic to Dogs!",
       images: [
         {
           url: "https://i.scdn.co/image/ab67616d0000b2730e68fefa1408603f54a4d664",
+          width: 640,
+          height: 640,
         }
-      ]
+      ],
+      album_type: "",
+      artists: [{ name: "Remi Wolf", href: "", id: "", type: "", uri: "" }],
+      release_date: "",
+      total_tracks: 10,
+      external_urls: { spotify: "" },
+      uri: "",
+      href: "",
+      id: "",
+      type: ""
     },
+    duration_ms: 1000,
+    explicit: false,
+    href: "",
+    uri: "",
+    type: "song",
+    popularity: 100,
   },
   
 ];
