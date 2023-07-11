77c5b5a6a011f81991238a287ecf9bb20449914d
Alex <ajoshua14@icloud.com>
2023-07-10 18:17:55 -0700

    Update player page to client component using server actinos

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -1,55 +1,43 @@
+'use client'
 
-import { getServerSession } from 'next-auth/next';
-import { redirect } from 'next/navigation';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { useEffect, useState } from "react";
 
-import { getCurrentlyPlaying } from "@/utilities/spotifyAPI";
-import { 
-  mapToCurrentlyPlaying, 
-  mapToSongs 
-} from "@/utilities/helper";
+import { getClientCurrentlyPlaying } from "@/utilities/spotifyAPI";
+
+import { msToTime } from "@/utilities/helper";
 
 import { 
   SpotifyItem, 
   CurrentlyPlaying, 
-  CurrentlyPlayingResponse 
 } from "@/types/spotifyTypes";
 
 import { SongCard } from "@/components/songCard";
 import { songs } from "@/data/songs";
 
-export default async function Player() {
-  const session = await getServerSession(authOptions);
+export default function Player() {
   
-  let currentlyPlaying: CurrentlyPlaying | null = null;
+  const [currentlyPlaying, setCurrentlyPlaying] = useState<CurrentlyPlaying | null>(null);
 
-  if (!session) {
-    console.log("No session found.")
-    redirect('/api/auth/signin?callbackUrl=/Player');
-  }
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      const currentlyPlaying = await getClientCurrentlyPlaying();
+      if (currentlyPlaying) {
+        setCurrentlyPlaying(currentlyPlaying);
+      }
+    }, 10000);
 
-  if (session.accessToken) {
-    try {
-    const currentlyPlayingData = await getCurrentlyPlaying(session.accessToken);
-    // const topTracksResponse = await Promise.all([topTracksData]);
-    // console.log(topTracks);
-    console.log(currentlyPlayingData);
-    currentlyPlaying = mapToCurrentlyPlaying(currentlyPlayingData);
-    
-    // console.log("_____________________________________");
-    // logTopTracks(topTracks);
-  } catch (error) {
-    console.log(error);
-  }
-  } else {
-    throw new Error("No access token found.");
-  }
+    // Clear timer on component unmount
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
 
   return (
     <main className="flex-1 p-16 flex flex-col items-center justify-between border-2 border-orange-500">
       <div className="flex flex-col">
         <div className="flex gap-4">
           <SongCard song={currentlyPlaying ? currentlyPlaying.item : songs[4]} />
+          <div>{msToTime(currentlyPlaying ? currentlyPlaying.progress_ms : 0)}</div>
         </div>
       </div>
     </main>
--- src/utilities/helper.ts
+++ src/utilities/helper.ts
@@ -47,3 +47,15 @@ export function logTopTracks(topTracks: SpotifyItem[]) {
     console.log(track.album.images[0]);
   }
 }
+
+export function msToTime(duration: number) {
+  const seconds = Math.floor((duration / 1000) % 60);
+  const minutes = Math.floor((duration / (1000 * 60)) % 60);
+
+  const formatedTimestamp = [
+    minutes.toString().padStart(2, '0'),
+    seconds.toString().padStart(2, '0'),
+  ].join(':');
+
+  return formatedTimestamp;
+}
