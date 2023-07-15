93e4fc6c17ad5b9e17c6aeb6991303e81434dd3a
Alex <ajoshua14@icloud.com>
2023-07-14 17:05:45 -0700

    Update profile page styling

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -6,22 +6,50 @@ import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function Settings() {
   const { data: session, status } = useSession();
+  let user = {
+    name: "John Doe",
+    email: "tester@gmail.com",
+  }
+  console.log(session, status);
 
-  if (status === "authenticated") {
-    return (
-    <>
-      Signed in as {session.user.email} <br />
-      <button onClick={() => signOut()}>Sign out</button> 
-    </>
-    )
+  const signinoutButton = () => {
+    if (status === "authenticated") {
+      return (
+        <button onClick={() => signOut()} className="w-full h-full rounded-full bg-red-700">
+          Sign out
+        </button> 
+      )
+    } else if (status === "loading") {
+      return (
+        <></>
+      )
+     } else {
+      return (
+        <button onClick={() => signIn()} className="w-full h-full rounded-full bg-green-700">
+          Sign in
+        </button> 
+      )
+    }
   }
 
   return (
-    <>
-      Not signed in <br />
-
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <main className="flex-1 p-4 flex flex-col items-center justify-around overflow-hidden border-2 border-yellow-500 rounded-lg">
+      <div className="flex w-full h-40 text-center justify-center items-center border-2 rounded-lg border-pink-500">
+        <span className="text-lg">
+          {status === "authenticated" ? 
+            `Signed in as ${user.name}` 
+            : status === "loading" ?
+            `Loading...`
+            : `Not signed in..` 
+          }
+        </span>
+      </div>
+      <div className="flex w-full h-full justify-center items-center border-2 border-teal-500 rounded-lg">
+        <div className="w-[180px] h-[80px] flex items-center justify-center text-2xl font-extralight">
+          {signinoutButton()}
+        </div>
+      </div>
+    </main>
   )
 
 
