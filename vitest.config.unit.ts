18e931e3b6c1da16d8336c9b7c0530c09a09349e
Alex <ajoshua14@icloud.com>
2023-09-07 16:07:20 -0700

    Allow test files to be declared closer to implementation code

--- vitest.config.unit.ts
+++ vitest.config.unit.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ['src/__tests__/**/*.test.ts', 'src/__tests__/**/*.test.tsx'],
+    include: ['src/__tests__/**/*.test.ts', 'src/__tests__/**/*.test.tsx', 'src/**/__tests__/*.test.ts'],
     environment: 'jsdom',
   },
   resolve: {
