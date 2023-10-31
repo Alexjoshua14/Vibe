e44ddf5dba7a4603f470aabfcd277af5e16bd1bf
Alex <ajoshua14@icloud.com>
2023-10-30 18:12:32 -0700

    Further implement createSession

--- src/lib/queue-session/session-management.ts
+++ src/lib/queue-session/session-management.ts
@@ -17,51 +17,79 @@ export async function createSession() {
 
   // Create session database objects
   try {
-    // const user = await prisma.user.findFirst({
-    //   where: {id: session.user.id },
-    //   include: {queue: true}
-    // })
+  // const user = await prisma.user.findFirst({
+  //   where: {id: session.user.id },
+  //   include: {queue: true}
+  // })
 
-    // if (user == null) {
-    //   throw new Error("No user found..")
-    // }
+  // if (user == null) {
+  //   throw new Error("No user found..")
+  // }
 
-    
-    
 
-    // const currentlyPlaying = await prisma.currentlyPlaying.create({
-    //   data: {
-    //     userId: session.user.id,
-    //     queueId: queue.id,
-    //     suggestedId: '12356'
-    //   }
-    // })
+  
+  
 
-    const user = await prisma.user.update({
-      where: { id: session.user.id },
-      data: {
-        queue: {
-          create: { }
-        },
-        suggested: {
-          create: {}
-        }
+  // const currentlyPlaying = await prisma.currentlyPlaying.create({
+  //   data: {
+  //     userId: session.user.id,
+  //     queueId: queue.id,
+  //     suggestedId: '12356'
+  //   }
+  // })
+
+  /** Ensure user doesn't have a currently playing already */
+  let user = await prisma.user.findFirst({
+    where: {
+      id: session.user.id
+    },
+    include: {
+      queue: true,
+      suggested: true,
+      currentlyPlaying: true
+    }
+  })
+
+  if (user == null)
+    throw new Error("No user found..")
+
+  if (user.queue) {
+    throw new Error("User already has queue")
+  }
+  if (user.suggested) {
+    throw new Error("User already has suggested")
+  }
+  if (user.currentlyPlaying) {
+    throw new Error("User already has currently playing")
+  }
+
+  /* Grab user from database */
+  user = await prisma.user.update({
+    where: { id: session.user.id },
+    data: {
+      queue: {
+        create: { }
       },
-    })
+      suggested: {
+        create: {}
+      }
+    },
+  })
 
-    if (user == null) {
-      throw new Error("No user found..")
-    }
-    if (user.queueId == null) {
-      throw new Error("Failed to instantiate queue object..")
-    }
-    if (user.suggestedId == null) {
-      throw new Error("Failed to instantiate suggested object..")
-    }
+  /* Ensure user was found and queue & suggested were created */
+  if (user == null) {
+    throw new Error("No user found..")
+  }
+  if (user.queueId == null) {
+    throw new Error("Failed to instantiate queue object..")
+  }
+  if (user.suggestedId == null) {
+    throw new Error("Failed to instantiate suggested object..")
+  }
 
-    if (user.queueId && user.suggestedId) {
-
-    await prisma.user.update({
+  if (user.queueId && user.suggestedId) {
+    /* Create a currently playing object in database for user */
+    user = await prisma.user.update({
       where: {id: session.user.id},
       data: {
         currentlyPlaying: {
@@ -71,12 +99,31 @@ export async function createSession() {
             suggestedId: user.suggestedId,
           }
         }
+      },
+      include: {
+        currentlyPlaying: true
       }
     })
+
+    /* Ensure currently playing was created */
+    if (!user.currentlyPlayingId) {
+      throw new Error("Failed to create currently playing")
+    }
+      let cp = prisma.currentlyPlaying.findFirst({
+        where: {
+          id: user.currentlyPlayingId,
+          userId: user.id
+        }
+      })
+
+      if (!cp) {
+        throw new Error("Failed to find just created currently playing object")
+      }
+      console.log(user.currentlyPlayingId)
+      return user.currentlyPlayingId
   } else {
     throw new Error("Invalid queueId or suggestedId")
   }
-
 } catch (err) {
   console.log(err)
 }
