44410cfaba211c243b127f6c21b078c550c5bfb4
Alex <ajoshua14@icloud.com>
2023-11-02 18:17:12 -0700

    Separate out image components into individual files

---
+++ src/components/Images/postImage.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react'
+import Image from 'next/image'
+
+interface postImageProps {
+  imageUrl: string
+  altText: string
+  [key: string]: any
+}
+
+const PostImage: FC<postImageProps> = ({ imageUrl, altText, ...props }) => {
+  return (
+    <div className="relative h-1/2 aspect-square flex center rounded-br overflow-hidden">
+      <Image
+        src={imageUrl}
+        alt={altText}
+        fill={true}
+        {...props}
+      />
+    </div>
+  )
+}
+
+export default PostImage
