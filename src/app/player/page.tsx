15dc3300f142ae2e4507c400e3867430eb342577
Alex <ajoshua14@icloud.com>
2023-11-05 15:32:13 -0800

    Use new callback buttons for more modular construction

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -1,12 +1,30 @@
-"use client"
+'use client'
+
 import React from "react"
 import Link from "next/link"
-import { redirect } from "next/navigation"
+import { useRouter } from "next/navigation"
 
+import { CallbackButton } from "@/components/buttons"
 import {
   createSession,
   destroySession,
 } from "@/lib/queue-session/session-management"
+
+const handleCreateSession = () => {
+  createSession().then((something) => {
+    console.log("Here is something from the promise: " + something)
+  })
+}
+
+const handleDestroySession = async () => {
+  const res = await destroySession()
+
+  if (res) {
+    console.log("Successfull created currently Playing object")
+  } else {
+    console.log("Looks like it failed to destroy a session..")
+  }
+}
 
 /**
  * Allows the user to either start a shared queue session or join one
@@ -22,21 +40,11 @@ export default function Player() {
 
   return (
     <main className="flex-1 p-4 flex flex-col items-center justify-around overflow-hidden">
-      <button
-        className="px-14 py-7 bg-teal-900 text-slate-50 text-2xl rounded"
-        onClick={() => createSession()}
-      >
-        Start a Session
-      </button>
+      <CallbackButton callback={() => handleCreateSession()} text="Start a Session" />
       <Link href="/player/join" className="px-14 py-7 bg-fuchsia-900 text-slate-50 text-2xl rounded">
         Join a Session
       </Link>
-      <button
-        className="px-14 py-7 bg-red-900 text-slate-50 text-2xl rounded"
-        onClick={() => destroySession()}
-      >
-        Delete a Session ⚠️
-      </button>
+      <CallbackButton callback={() => handleDestroySession()} text="Delete a Session ⚠️" />
     </main>
   )
 }
