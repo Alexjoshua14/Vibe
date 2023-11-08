198a9341a6d7a80d71608192b555ebed86177eb4
Alex <ajoshua14@icloud.com>
2023-11-07 19:04:52 -0800

    Connect searching boolean in redux store to buttons and styling

--- src/components/cards/currentlyPlayingCard.tsx
+++ src/components/cards/currentlyPlayingCard.tsx
@@ -1,8 +1,13 @@
-import React, { FC, HTMLAttributes, useRef } from 'react'
-import { Artist, Song } from '@prisma/client';
+'use client'
+
+import React, { FC, HTMLAttributes, useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { easeInOut, motion } from 'framer-motion';
 import Image from "next/image";
 
 import { cn } from '@/lib/utils';
+import { Context } from '@/lib/validators/context';
+import { setSearching } from '@/redux/reducers/search';
 import { msToTime } from '@/utilities/helper';
 
 import ScrollingText from '../scrollingText';
@@ -27,45 +32,67 @@ interface CardProps extends HTMLAttributes<HTMLDivElement> {
 export const Card: FC<CardProps> = ({ image, songName, artists, duration, progress, className, ...props }) => {
   const songNameRef = useRef(null)
   const artistNameRef = useRef(null)
+  const { searching } = useSelector((state: Context) => state.search)
+  const dispatch = useDispatch()
 
   return (
-    <div
-      className={cn("flex flex-col items-center justify-center gap-4 min-h-[270px] max-h-[600px] min-w-[180px] w-full max-w-[400px] aspect-[2/3] text-center", className)}
-    >
-      <div className="relative max-h-2/3 w-full aspect-square rounded overflow-clip glassmorphism">
-        <Image
-          src={image.url}
-          alt={image.alt}
-          fill
-          quality={image.quality ?? 75}
-          className="object-contain aspect-square"
-        />
-      </div>
-      <div className="flex flex-col w-full gap-2 px-4">
-        <div className="max-w-full px-4">
-          <div ref={songNameRef} className="max-w-full overflow-hidden">
-            <ScrollingText containerRef={songNameRef} text={songName} className="text-primary text-xl leading-tight" />
+    <>
+      <motion.div
+        animate={{
+          flexDirection: searching ? 'row' : 'column',
+          textAlign: searching ? 'left' : 'center'
+        }}
+        transition={{ duration: 1, ease: easeInOut }}
+        className={
+          cn(
+            `flex flex-col items-center justify-center gap-4 
+            min-h-[270px] max-h-[600px] min-w-[180px] w-full max-w-[400px] aspect-[2/3]`,
+            className
+          )
+        }
+      >
+        <motion.div
+          animate={{ maxHeight: searching ? '100%' : '66.667%' }}
+          className="relative w-full aspect-square rounded overflow-clip glassmorphism"
+        >
+          {image?.url ?
+            (
+              <Image
+                src={image.url}
+                alt={image.alt}
+                fill
+                quality={image.quality ?? 75}
+                className="object-contain aspect-square"
+              />
+            ) :
+            (
+              <div className="w-full h-full bg-teal-700 glassmorphism rounded" />
+            )
+          }
+
+        </motion.div>
+
+        <div className={`flex flex-col w-full gap-2 ${searching ? 'px-2' : 'px-4'}`}>
+          <div className={`max-w-full ${searching ? 'px-0' : 'px-4'}`}>
+            <div ref={songNameRef} className="max-w-full overflow-hidden">
+              <ScrollingText containerRef={songNameRef} text={songName} className={`text-primary ${searching ? 'text-base' : 'text-xl'} leading-tight`} />
+            </div>
+            <div ref={artistNameRef} className="max-w-full overflow-hidden">
+              <ScrollingText containerRef={artistNameRef} text={artists} className={`text-secondary ${searching ? 'text-sm' : 'text-lg'} font-light leading-tight`} />
+            </div>
           </div>
-          <div ref={artistNameRef} className="max-w-full overflow-hidden">
-            <ScrollingText containerRef={artistNameRef} text={artists} className="text-secondary text-lg leading-tight" />
+          <div className="w-full flex items-center justify-between gap-2 text-tertiary text-xs">
+            <p>
+              {msToTime(progress.time)}
+            </p>
+            <Progress value={progress.percentage} />
+            <p>
+              {msToTime(duration)}
+            </p>
           </div>
-          {/* <p className="text-primary text-xl h-[1.75rem]">
-            {songName}
-          </p>
-          <p className="text-secondary text-lg h-[1.75rem]">
-            {artists}
-          </p> */}
         </div>
-        <div className="w-full flex items-center justify-between gap-2 text-tertiary text-xs">
-          <p>
-            {msToTime(progress.time)}
-          </p>
-          <Progress value={progress.percentage} />
-          <p>
-            {msToTime(duration)}
-          </p>
-        </div>
-      </div>
-    </div>
+      </motion.div>
+      <button onClick={() => dispatch(setSearching({ searching: !searching }))}>Toggle</button>
+    </>
   )
 }
--- src/lib/validators/context.ts
+++ src/lib/validators/context.ts
@@ -6,9 +6,14 @@ import { CurrentlyPlayingSchema } from './spotify'
 export const StatusSchema = z.enum(['IDLE', 'HOST', 'MEMBER'])
 export type Status = z.infer<typeof StatusSchema>
 
+export const SearchSchema = z.object({
+  searching: z.boolean(),
+})
+
 export const ContextSchema = z.object({
   currentlyPlaying: CurrentlyPlayingSchema,
-  status: StatusSchema
+  status: StatusSchema,
+  search: SearchSchema,
 })
 
 export type Context = z.infer<typeof ContextSchema>
