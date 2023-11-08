7aa1413ffab846b961522772a876da8788ee9fb2
Alex <ajoshua14@icloud.com>
2023-11-07 17:25:50 -0800

    Add carousel elements that will hold queue and suggested songs for host

--- src/components/sharedSession.tsx
+++ src/components/sharedSession.tsx
@@ -6,9 +6,10 @@ import { useSelector } from 'react-redux'
 import { getUser } from '@/lib/prisma/user'
 import { Context } from '@/lib/validators/context'
 
-import CurrentlyPlaying from './currentlyPlaying'
 import Search from './search/search'
+import CurrentlyPlaying from './currentlyPlaying'
 import { LeaveSession } from './sessionButtons'
+import SongCarousel from './songCarousel'
 
 interface sharedSessionProps {
 
@@ -21,9 +22,21 @@ const SharedSession: FC<sharedSessionProps> = ({ }) => {
 
   if (status === 'HOST') {
     return (
-      <div>
+      <div className="w-full h-full">
         <h1>Host</h1>
-        <CurrentlyPlaying />
+        <div className="w-full h-full grid grid-cols-2 grid-rows-1 items-center">
+          <div className="h-full col-span-1 p-20 flex items-center justify-center">
+            <CurrentlyPlaying />
+          </div>
+          <div className="h-3/5 col-span-1 flex flex-col gap-8">
+            <div className="w-full h-1/2 flex items-center justify-end">
+              <SongCarousel header="Queue" />
+            </div>
+            <div className="w-full h-1/2 flex items-center justify-end">
+              <SongCarousel header="Suggested" />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
