2437eab9b98e92d159806bf6f6e9a32ad33c108a
Alex <ajoshua14@icloud.com>
2023-11-09 19:20:44 -0800

    Implement functions to add and remove user from currentlyPlaying database object

--- src/lib/prisma/currentlyPlaying.ts
+++ src/lib/prisma/currentlyPlaying.ts
@@ -190,3 +190,57 @@ export async function updateCurrentlyPlayingDB(currentlyPlaying: CurrentlyPlayin
 
   return null
 }
+
+export async function addUserToSession(sessionId: string) {
+  const user = await getServerSession(authOptions)
+  if (user === null) {
+    console.log("No user found..")
+    return false
+  }
+
+  const cp = await prisma.currentlyPlaying.findFirst({ where: { id: sessionId }, select: { members: true }})
+
+  if (cp === null) {
+    console.log("No session found with id matching the one provided..")
+    return false
+  }
+
+  // Adds user to the currently playing database object's member array
+  await prisma.currentlyPlaying.update({
+    where: {
+      id: sessionId
+    },
+    data: {
+      members: {
+        connect: {
+          id: user.user.id
+        }
+      }
+    }
+  })
+    
+}
+
+/**
+ * Checks if the user has a currently playing session in the database
+ */
+export async function reconnect() {
+  console.log("Searching for previous session..")
+  const user = await getServerSession(authOptions)
+  if (user === null) {
+    console.log("No user found..")
+    return {cpID: null, prevStatus: 'IDLE'}
+  }
+
+  const userDB = await prisma.user.findFirst({where: {id: user.user.id}, include: { currentlyPlaying: true }})
+
+  if (userDB?.currentlyPlaying === null) {
+    console.log("No session found with id matching the one provided..")
+    return {cpID: null, prevStatus: null}
+  }
+
+  const prevStatus: 'HOST' | 'MEMBER' = userDB?.currentlyPlaying.userId === user.user.id ? 'HOST' : 'MEMBER'
+
+  return {cpID: userDB?.currentlyPlayingId, prevStatus}
+
+}
