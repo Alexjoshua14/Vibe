5d32f93073acda007a64c0357978157caa37c0bd
Alex <ajoshua14@icloud.com>
2023-11-14 15:43:42 -0800

    Move fetching of available sessions into one server call

---
+++ src/lib/queue-session/sessionSetup.ts
@@ -0,0 +1,38 @@
+"use server"
+
+import { getServerSession } from "next-auth"
+
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import prisma from "@/lib/prisma"
+
+import { getUserDeep } from "../prisma/user"
+
+// Replaces lsitAllSessions() from 'session-maanagement.ts'
+async function fetchSessions() {
+  
+}
+
+export interface AvailableSession {
+  id: string,
+  hostName: string
+}
+
+export async function getAvailableSessions() {
+  const user = await getServerSession(authOptions)
+  if (!user) {
+    throw new Error("No user found..")
+  }
+
+  const avaialableSessions = await prisma.currentlyPlaying.findMany({select: { id: true, userId: true }})
+
+  let sessionWithUserPromises = avaialableSessions.map(async (session) => {
+    const user = await prisma.user.findFirst({ where: { id: session.userId }, select: { name: true } })
+    let hostName = user?.name ?? "Unknown"
+    return { id: session.id, hostName: hostName } as AvailableSession
+    
+  })
+  
+  const sessionsWithUsers = await Promise.all(sessionWithUserPromises)
+
+  return sessionsWithUsers
+}
