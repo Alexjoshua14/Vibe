bafb2104c3f21c97cb7f22307137ccd0774f176a
Alex <ajoshua14@icloud.com>
2023-11-15 17:50:54 -0800

    Remove tooltip for now because it has an error with props somewhere further down the dependency chain

--- src/components/buttons/likeButton.tsx
+++ src/components/buttons/likeButton.tsx
@@ -6,8 +6,6 @@ import { AnimatePresence, motion } from "framer-motion"
 
 import { likeSong, unlikeSong } from "@/utilities/spotifyAPI"
 
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip"
-
 interface LikeButtonProps {
   songId: string
   songName: string
@@ -28,50 +26,41 @@ const LikeButton: FC<LikeButtonProps> = ({ songId, songName }) => {
 
   return (
     <div className="relative w-8 max-w-full min-h-[30px] h-full">
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger>
-            <AnimatePresence>
-              {liked ? (
-                <motion.button
-                  key={`liked-${songName}`}
-                  onClick={handleUnlike}
-                  animate={{
-                    scale: [0.2, 1.4, 1],
-                    opacity: [0.4, 1.4, 1],
-                    translate: ["-50% -50%"],
-                  }}
-                  exit={{ scale: 0, opacity: 0 }}
-                  transition={{ duration: 0.9, ease: "easeInOut" }}
-                  className="z-20 absolute top-1/2 left-1/2"
-                >
-                  <IoIosHeart size={24} className="text-red-500" />
-                </motion.button>
-              ) : (
-                <motion.button
-                  key={`unliked-${songName}`}
-                  onClick={handleLike}
-                  className="z-10 absolute top-1/2 left-1/2"
-                  animate={{
-                    scale: 1,
-                    opacity: 1,
-                    translate: ["-50% -50%"],
-                  }}
-                  exit={{ scale: 4, opacity: 0 }}
-                  transition={{ duration: 0.4, ease: "easeInOut" }}
-                >
-                  <IoIosHeartEmpty size={24} />
-                </motion.button>
-              )}
-            </AnimatePresence>
-          </TooltipTrigger>
-          <TooltipContent sideOffset={10} className="glassmorphism-tertiary">
-            <p>
-              {`${liked ? "Remove" : "Add"} ${songName} to your Spotify library`}
-            </p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+
+      <AnimatePresence>
+        {liked ? (
+          <motion.button
+            key={`liked-${songName}`}
+            onClick={handleUnlike}
+            animate={{
+              scale: [0.2, 1.4, 1],
+              opacity: [0.4, 1.4, 1],
+              translate: ["-50% -50%"],
+            }}
+            exit={{ scale: 0, opacity: 0 }}
+            transition={{ duration: 0.9, ease: "easeInOut" }}
+            className="z-20 absolute top-1/2 left-1/2"
+          >
+            <IoIosHeart size={24} className="text-red-500" />
+          </motion.button>
+        ) : (
+          <motion.button
+            key={`unliked-${songName}`}
+            onClick={handleLike}
+            className="z-10 absolute top-1/2 left-1/2"
+            animate={{
+              scale: 1,
+              opacity: 1,
+              translate: ["-50% -50%"],
+            }}
+            exit={{ scale: 4, opacity: 0 }}
+            transition={{ duration: 0.4, ease: "easeInOut" }}
+          >
+            <IoIosHeartEmpty size={24} />
+          </motion.button>
+        )}
+      </AnimatePresence>
+
     </div>
   )
 }
