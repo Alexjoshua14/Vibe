2f1f714d9578f6535d46b84b515b64441c90c125
Alex <ajoshua14@icloud.com>
2023-11-20 19:15:11 -0800

    Tweak container styling

--- src/components/bottomNav.tsx
+++ src/components/bottomNav.tsx
@@ -7,7 +7,7 @@ import { FeedButton, PlayerButton, ProfileButton } from "./buttons"
 
 export const BottomNav = () => {
   return (
-    <nav className="fixed bottom-5 left-1/2 -translate-x-1/2 h-14 sm:h-20 px-2 py-1 text-center bg-primary glassmorphism-primary rounded">
+    <nav className="fixed bottom-5 left-1/2 -translate-x-1/2 h-14 sm:h-20 px-2 py-1 text-center glassmorphism-primary rounded">
       <ul className="w-full h-full flex gap-2 items-center justify-around">
         <li>
           <PlayerButton className="glassmorphism-secondary-interactive bg-primary px-6 py-3 rounded hover:scale-110 transiton-all subpixel-antialiased" />
