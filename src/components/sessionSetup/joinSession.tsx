d578248c6e2ebf4d269daf1b71f7e65c02023649
Alex <ajoshua14@icloud.com>
2023-11-20 16:40:32 -0800

    Create components for creating/joining sessions

---
+++ src/components/sessionSetup/joinSession.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { FC, useEffect, useState } from 'react'
+
+import { useSessionManagement } from '@/app/hooks/useSessionManagement'
+import { AvailableSession, getAvailableSessions } from '@/lib/queue-session/sessionSetup'
+
+import { JoinSessionButton } from '../buttons'
+
+interface JoinSessionProps {
+
+}
+
+const JoinSession: FC<JoinSessionProps> = ({ }) => {
+  const { handleJoinSession } = useSessionManagement()
+  const [availableSessions, setAvailableSessions] = useState<
+    AvailableSession[]
+  >([])
+  const [loadingAvailableSessions, setLoadingAvailableSessions] =
+    useState<boolean>(true)
+
+  useEffect(() => {
+    const fetchAvailableSessions = async () => {
+      const availableSessions = await getAvailableSessions()
+      setAvailableSessions(availableSessions)
+      setLoadingAvailableSessions(false)
+    }
+
+    fetchAvailableSessions()
+  })
+  return (
+    <div className="h-[300px] w-[400px] flex flex-col items-center justify-center gap-4">
+      {availableSessions.length > 0 ? (
+        availableSessions.map((session) => (
+          <JoinSessionButton
+            key={session.id}
+            sessionId={session.id}
+            handleJoinSession={handleJoinSession}
+            text={`Join ${session.hostName}'s session!`}
+          />
+        ))
+      ) : (
+        <p>No sessions available at the moment..</p>
+      )}
+    </div>
+  )
+}
+
+export default JoinSession
