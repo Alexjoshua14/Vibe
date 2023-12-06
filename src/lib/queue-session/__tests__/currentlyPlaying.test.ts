b99995cb47336a20fdd4ae198536a43651e0ef98
Alexander Joshua <59298565+Alexjoshua14@users.noreply.github.com>
2023-12-06 01:39:32 -0800

    Update currentlyPlaying.test.ts
    Outline tests to be written

---
+++ src/lib/queue-session/__tests__/currentlyPlaying.test.ts
@@ -0,0 +1,7 @@
+/** Test file for Currently Playing functions **/
+/** 
+* Functions to be tested
+* - getCurrentlyPlaying_Host - Ensure it connects to spotif, handles res, and prepares payload
+* - getCurrentlyPlaying_Member - Ensure proper params passed and proper communication to prisma client
+*
+**/
