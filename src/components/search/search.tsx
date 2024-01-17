505631abbc5c678918ad3872b5039abba236d06e
Alex <ajoshua14@icloud.com>
2024-01-16 16:57:03 -0800

    Clean up imports

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -15,10 +15,6 @@ import { SearchField } from "./searchField"
 
 interface searchProps { }
 
-/**
- * TODO - clean up ui presentation
- *
- */
 const Search: FC<searchProps> = ({ }) => {
   /* Search Spotify for Tracks */
   const {
