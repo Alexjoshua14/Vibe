20992977516bf565fe1167bc008bc1606b9a24dc
Alex <ajoshua14@icloud.com>
2024-02-01 17:49:45 -0800

    Begin crafting API endpoints for calling ranking backend server from nextjs environment

--- src/api/index.py
+++ src/api/index.py
@@ -1,6 +1,54 @@
-from flask import Flask
+from src.api.ranking.ranking3 import Rankings, testing
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route("/api/python")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return testing()
+  
+@app.route("/api/sample", methods=["POST"])
+def sample():
+  print("Request received...")
+  try:
+    print("Request received and parsing JSON data...")
+    json_data = request.get_json()
+    
+    print("JSON data: " + json_data)
+    
+    if not json_data:
+      return jsonify({"error": "No JSON data provided"}), 400
+    
+    return jsonify(json_data)
+  except Exception as e:
+    print("Exception: " + str(e))
+    return jsonify({"error": "An error occurred"}), 500
+  
+@app.route("/api/ranking", methods=["POST"])
+def compute_ranking():
+  try:
+    print("Request received and parsing JSON data...")
+    json_data = request.get_json()
+    
+    print("JSON data: " + json_data)
+    
+    if not json_data:
+      return jsonify({"error": "No JSON data provided"}), 400
+    
+    limit = json_data.get("limit")
+    
+    if not limit:
+      return jsonify({"error": "No limit provided"}), 400
+    
+    items = json_data.get("items")
+    
+    if not items:
+      return jsonify({"error": "No items provided"}), 400
+    
+    rankings = Rankings(limit)
+  
+  except Exception as e:
+    print("Exception: " + str(e))
+    return jsonify({"error": "An error occurred"}), 500
+    
+    
+  
