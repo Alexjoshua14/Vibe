91932ffd299b91c1f4164919d69bf5340fb28441
Alex <ajoshua14@icloud.com>
2024-02-03 13:01:12 -0800

    Comment out new feature code for now

--- src/app/charts/page.tsx
+++ src/app/charts/page.tsx
@@ -19,12 +19,12 @@ export default async function Charts() {
   // const res = await getRankings(sampleData)
   // console.log(res)
 
-  const res = await computeGroupTopTracks()
-  console.log(res)
+  // const res = await computeGroupTopTracks()
+  // console.log(res)
 
-  const tracks = res.map((rt: [string, number]) => {
-    return Track(decodeURI(rt[0]), rt[1])
-  })
+  // const tracks = res.map((rt: [string, number]) => {
+  //   return Track(decodeURI(rt[0]), rt[1])
+  // })
 
   return (
     <main className="w-full min-h-screen flex items-center justify-center">
@@ -33,9 +33,9 @@ export default async function Charts() {
           Charts
         </h1>
         <div className="min-w-[400px] w-full min-h-[40vh] flex-1 flex flex-col gap-4 bg-pink-200">
-          {
+          {/* {
             tracks
-          }
+          } */}
         </div>
       </section>
     </main>
