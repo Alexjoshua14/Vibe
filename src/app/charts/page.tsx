2f42a5fe90b83e99e8bb41bde37862db0acf1adb
Alex <ajoshua14@icloud.com>
2024-02-03 12:24:34 -0800

    Begin swapping out initial test functions for more proper ones

--- src/app/charts/page.tsx
+++ src/app/charts/page.tsx
@@ -1,8 +1,10 @@
 
+import { computeGroupTopTracks, decodeURI } from "@/lib/topCharts/topTracks"
 
-async function sample(data: any) {
+import { sampleData } from "./sampleData"
 
-  const res = await fetch('http://localhost:3000/api/flask/sample', {
+const getRankings = async (data: any) => {
+  const res = await fetch('http://localhost:3000/api/flask/ranking', {
     method: 'POST',
     headers: {
       "Content-Type": "application/json"
@@ -13,21 +15,30 @@ async function sample(data: any) {
   return res.json()
 }
 
-async function helloWorld() {
-  const res = await fetch('http://localhost:3000/api/flask/hello')
-  return res.json()
+const Track = (name: string, score: number) => {
+  return (
+    <div className="flex items-center justify-between gap-8">
+      <p>
+        {name}
+      </p>
+      <p>
+        {score}
+      </p>
+    </div>
+  )
 }
 
 export default async function Charts() {
-  const hey = await helloWorld()
-  console.log(`Hello world response: ${hey}`)
 
-  const jsonSample = {
-    test: "hello there"
-  }
+  // const res = await getRankings(sampleData)
+  // console.log(res)
 
-  const data = await sample(jsonSample)
-  console.log(data)
+  const res = await computeGroupTopTracks()
+  console.log(res)
+
+  const tracks = res.map((rt: [string, number]) => {
+    return Track(decodeURI(rt[0]), rt[1])
+  })
 
   return (
     <main className="w-full min-h-screen flex items-center justify-center">
@@ -35,8 +46,10 @@ export default async function Charts() {
         <h1 className="text-5xl">
           Charts
         </h1>
-        <div className="min-w-[400px] w-full min-h-[40vh] flex-1 bg-pink-200">
-          {/* {data} */}
+        <div className="min-w-[400px] w-full min-h-[40vh] flex-1 flex flex-col gap-4 bg-pink-200">
+          {
+            tracks
+          }
         </div>
       </section>
     </main>
